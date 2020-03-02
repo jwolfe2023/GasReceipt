c020ed348284e11be87372939ad1fda4c7776e85
James Wolfe <james.wolfe.2023@student.lhschools.org>
2020-03-02 12:01:33 -0500

    IntelliJ Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,40 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+------------------------------+");
+        System.out.println("|   Welcome To The Marathon    |");
+        System.out.println("|         Gas Station          |");
+        System.out.println("|                              |");
+        System.out.println("|  Address: 131 S Sunbury Rd,  |");
+        System.out.println("|    Westerville, OH 43081     |");
+        System.out.println("|                              |");
+        System.out.println("|     Date   01/10/2020        |");
+        System.out.println("|     Time   12:09 PM          |");
+        System.out.println("|     Auth#  123456            |");
+        System.out.println("|             Visa             |");
+        System.out.println("|        Account Number        |");
+        System.out.println("|     ####-####-####-##32      |");
+        System.out.println("|         James Wolfe          |");
+        System.out.println("|                              |");
+        System.out.println("|                              |");
+        System.out.println("|   Pump    Product    $/G     |");
+        System.out.println("|    04      UNLD     $2.9     |");
+        System.out.println("|                              |");
+        System.out.println("|   Gallons           Total    |");
+        System.out.println("|   20.69            $60.00    |");
+        System.out.println("|                              |");
+        System.out.println("|          THANK YOU!          |");
+        System.out.println("|        HAVE A NICE DAY       |");
+        System.out.println("|                              |");
+        System.out.println("|                              |");
+        System.out.println("|                              |");
+        System.out.println("|                              |");
+        System.out.println("+------------------------------+");
+
+
+
+
+    }
+}
